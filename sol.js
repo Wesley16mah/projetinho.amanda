4c3d582e60bfcd2ae9822a2f986469b05367371a
wesley.andrade <125272674+Wesley16mah@users.noreply.github.com>
2024-06-21 13:31:20 -0300

    Add files via upload

---
+++ sol.js
@@ -0,0 +1,142 @@
+const characters = [
+    {
+      nome: "Bob Esponja",
+      img: "bobesponja.png"
+    },
+    {
+      nome: "Buzz",
+      img: "buzz.jpeg"
+    },
+    {
+      nome: "Cebolinha",
+      img: "Cebolinha.png"
+    },
+    {
+      nome: "Finn",
+      img: "finn.jpeg"
+    },
+    {
+      nome: "Gumball",
+      img: "gumball.png"
+    },
+    {
+      nome: "Jake",
+      img: "jake.png"
+    },
+    {
+      nome: "Alegria",
+      img: "joyalegria.jpg"
+    },
+    {
+      nome: "Marceline",
+      img: "marceline.webp"
+    },
+    {
+      nome: "Mickey mouse",
+      img: "mickey mouse.jpeg"
+    },
+    {
+      nome: "Mônica",
+      img: "monica.png"
+    },
+    {
+      nome: "Nemo",
+      img: "nemo.png"
+    },
+    {
+      nome: "Princesa Jujuba",
+      img: "PrincesaJujuba.webp"
+    },
+    {
+      nome: "Raiva",
+      img: "Raiva.webp"
+    },
+    {
+      nome: "Rapunzel",
+      img: "rapunzel.jpeg"
+    },
+    {
+      nome: "Scooby",
+      img: "Scooby.jpg"
+    },
+    {
+      nome: "Shrek",
+      img: "shrek.jpeg"
+    },
+    {
+      nome: "Moana",
+      img: "moana.jpeg"
+    },
+    {
+      nome: "Goku",
+      img: "goku.jpeg"
+    },
+    {
+      nome: "Minion",
+      img: "minion.png"
+    },
+    {
+      nome: "Gru",
+      img: "gru.webp"
+    },
+    {
+      nome: "Goku",
+      img: "goku.jpeg"
+    },
+    {
+      nome: "Homem Aranha",
+      img: "homemaranha.jpeg"
+    },
+    {
+      nome: "Moana",
+      img: "moana.jpeg"
+    },
+    {
+      nome: "Woody",
+      img: "woody.webp"
+    },
+    {
+      nome: "Elsa",
+      img: "elza.png"
+    },
+
+  ];
+  
+  var indexNumber = 0;
+  
+  function newCharacter() {
+    var imagem = document.getElementById("imagem");
+    var display = document.getElementById("resposta").style.display;
+    indexNumber = Math.floor(Math.random() * characters.length);
+    imagem.src = characters[indexNumber].img ;
+    if (display == "block") {
+      changeDisplay("resposta");
+    }
+  }
+  
+  newCharacter();
+  
+  function changeDisplay(el) {
+    var display = document.getElementById(el).style.display;
+    if (display == "none") document.getElementById(el).style.display = "block";
+    else document.getElementById(el).style.display = "none";
+  }
+  
+  function discover() {
+    var inputName = document.getElementById("inputname").value;
+  
+    if (inputName.length) {
+      if (inputName.toLowerCase() == characters[indexNumber].nome.toLowerCase()) {
+        document.getElementById("resposta").style.display = "block";
+        document.getElementById("resposta").innerHTML = "Você acertou 🥳";
+      } else {
+        changeDisplay("resposta");
+        document.getElementById("resposta").style.display = "block";
+        document.getElementById("resposta").innerHTML = "Tente outra vez";
+      }
+    } else {
+      changeDisplay("resposta");
+      document.getElementById("resposta").innerHTML = "Coloque algum nome válido";
+    }
+  }
+  
